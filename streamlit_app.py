5afa6c192c0b729123c0d4449a75c02bbb8ad33e
EKELLY79 <136089540+EKELLY79@users.noreply.github.com>
2023-06-09 14:08:52 +0100

    Update streamlit_app.py
    New Option Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
+🥣 🥗 🐔 🥑🍞
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Bioled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Bioled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
